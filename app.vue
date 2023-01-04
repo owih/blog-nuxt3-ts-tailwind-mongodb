d865005c0eec099a93c6fed4b2ec02cf97e4f313
Golovatenko D.F <golovatenko@intervolga.ru>
2023-01-04 19:34:56 +0300

    init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="{dark: darkMode}">
+    <NuxtPage />
+  </div>
+</template>
+<script setup lang="ts">
+const darkMode = ref(false)
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex bg-teal-">
+    init
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
